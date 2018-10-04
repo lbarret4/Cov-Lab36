25f7a6ec5c6ebd6f4cdb086b0102c7dee974ef59
lbarret4 <llewellynbar4@gmail.com>
2018-10-04 18:27:13 -0400

    resized form to not take full with of container

--- client/src/components/auth/login.jsx
+++ client/src/components/auth/login.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import {Link} from 'react-router-dom';
 import * as userService from '../../services/user';
 import { Redirect } from 'react-router-dom';
 import IndeterminateProgress from '../utilities/indeterminateProgress';
@@ -17,25 +18,25 @@ class Login extends Component {
 
     componentDidMount() {
         userService.checkLogin()
-        .then((loggedIn) => {
-            if (loggedIn) {
-                this.setState({ redirectToReferrer: true, checkingLogin: false });
-            } else {
-                this.setState({ checkingLogin: false });
-            }
-        });
+            .then((loggedIn) => {
+                if (loggedIn) {
+                    this.setState({ redirectToReferrer: true, checkingLogin: false });
+                } else {
+                    this.setState({ checkingLogin: false });
+                }
+            });
     }
 
     login(e) {
         e.preventDefault();
         userService.login(this.state.email, this.state.password)
-        .then(() => {
-            this.setState({ redirectToReferrer: true });
-        }).catch((err) => {
-            if (err.message) {
-                this.setState({ feedbackMessage: err.message });
-            }
-        });
+            .then(() => {
+                this.setState({ redirectToReferrer: true });
+            }).catch((err) => {
+                if (err.message) {
+                    this.setState({ feedbackMessage: err.message });
+                }
+            });
     }
 
     handleEmailChange(value) {
@@ -47,37 +48,47 @@ class Login extends Component {
     }
 
     render() {
-       const { from } = this.props.location.state || { from: { pathname: '/' } };
-       const { redirectToReferrer, checkingLogin } = this.state;
+        const { from } = this.props.location.state || { from: { pathname: '/' } };
+        const { redirectToReferrer, checkingLogin } = this.state;
 
-       if (checkingLogin) {
-           return <IndeterminateProgress message="Checking Login Status..." />;
-       }
-       if (redirectToReferrer) {
-           return (
-               <Redirect to={from} />
-           );
-       }
+        if (checkingLogin) {
+            return <IndeterminateProgress message="Checking Login Status..." />;
+        }
+        if (redirectToReferrer) {
+            return (
+                <Redirect to={from} />
+            );
+        }
 
-       return (
-           <Fragment>
-                <p>You must be logged in to view this page.</p>
-                <form onSubmit={(e) => this.login(e)}>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input id="email" className="form-control" type="email" onChange={(e) => this.handleEmailChange(e.target.value)} required /> 
+        return (
+            <Fragment>
+                <div className='row'>
+                    <div className='col-md-6 offset-md-3'>
+                        <p className='text-danger text-center'>You must be logged in to view this page.</p>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input id="password" className="form-control" type="password" onChange={(e) => this.handlePasswordChange(e.target.value)} required /> 
+
+                </div>
+                <div className='row'>
+                    <div className='col-md-6 offset-md-3'>
+                        <form onSubmit={(e) => this.login(e)}>
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input id="email" className="form-control" type="email" onChange={(e) => this.handleEmailChange(e.target.value)} required />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input id="password" className="form-control" type="password" onChange={(e) => this.handlePasswordChange(e.target.value)} required />
+                            </div>
+                            {this.state.feedbackMessage ? (
+                                <p>{this.state.feedbackMessage}</p>
+                            ) : null}
+                            <input type="submit" value="Login" className="btn btn-primary btn-block" />
+                            <Link to='/'> Create an Account</Link>
+                        </form>
                     </div>
-                    {this.state.feedbackMessage ? (
-                        <p>{ this.state.feedbackMessage }</p>
-                    ): null}
-                    <input type="submit" value="Login" className="btn btn-primary" />
-                </form>
+                </div>
             </Fragment>
-       );
+        );
     }
 }
 
